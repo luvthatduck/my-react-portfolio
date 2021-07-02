80f9ce44cca2ff23c3d71c013429a8a9c16d941f
Janell Myrick <76677736+luvthatduck@users.noreply.github.com>
2021-07-02 15:22:13 -0600

    sizing icons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,9 @@ function Footer() {
       <p>
       Made by Myrick <br></br>
 
-      <a href="https://github.com/luvthatduck" target="_blank"><i class="fab fa-github icon is-large" href></i></a>
-      <a href="https://twitter.com/luvthatduck2" target="_blank"><i class="fab fa-twitter icon is-large" href></i></a>  
-      <a href="https://www.linkedin.com/in/janell-myrick-2580a0203/" target="_blank"><i class="fab fa-linkedin icon is-large" href></i></a>  
+      <a href="https://github.com/luvthatduck" target="_blank"><i class="fab fa-github icon fa-5x fa-fw" href></i></a>
+      <a href="https://twitter.com/luvthatduck2" target="_blank"><i class="fab fa-twitter icon fa-5x fa-fw" href></i></a>  
+      <a href="https://www.linkedin.com/in/janell-myrick-2580a0203/" target="_blank"><i class="fab fa-linkedin icon fa-5x fa-fw" href></i></a>  
       </p>
     </footer>
   );
