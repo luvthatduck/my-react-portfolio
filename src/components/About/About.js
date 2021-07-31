ce2e307f2d423ebfa5c6ee76de8e2465df033d8b
Janell Myrick <76677736+luvthatduck@users.noreply.github.com>
2021-07-30 22:09:36 -0600

    updating resume page and about me

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import ProfilePic from '../../assets/large/JanellMyrickProfilePic.jpg'
+import Image from 'react-bootstrap/Image'
 
 function About() {
   return (
     <section className="my-5" class="hero ">
       <h1 id="about" class="about"></h1> 
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-start', }}>
-    
-          <figure class="image is-128x128 ">
-            <img class="profilepic is-rounded mb-2" src={ProfilePic} alt="profile picture" />
-          </figure>
-
-       
-      </div>
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', }}>
+     
+      <div style={{ display: 'flex', flexWrap: 'wrap-reverse', justifyContent: 'center', alignItems: 'center', }}>
+        
         <div class="card2 is-align-content-center mt-3">
-          <div class="card-content">
-            <div class="content">
+          <div class="card-content" style={{ display: 'flex', flexWrap: 'wrap-reverse', justifyContent: 'center', alignItems: 'center', }}>
+            
+          <figure class="image is-128x128 ">
+            <Image class="profilepic " src={ProfilePic} alt="profile picture" roundedCircle></Image>
+          </figure>
+            <div class="content" style={{ display: 'flex',  justifyContent: 'center', alignItems: 'right', }}>
               Hello there! <br></br>
                I am a STEM teacher and Full Stack web developer. I love educating people and help them reach their potential. I LOVE science of all types but especially microbiology and entomology. I'm a vivacious reader of fiction, connoisseur of all chocolates and try to eat as much Mexican food as I can get.
             </div>
--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,34 +1,70 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 function Resume() {
 
   return (
-    <section className="section resume columns is-centered">
-      <div>
-        <h1 className="title resume  has-text-centered">Resume</h1>
-        <h2 className="subtitle">
-        
-          <div class="box">
-            Download my Resume <a className="resume-anchor" href={require('../../assets/janellmyrickResume.pdf').default} target="_blank" download>here.</a>
-          </div>
-          <h1 className="title resume  has-text-centered">Skills</h1>
-          <div class="box">
-            <ul>
-              <li><i class="fas fa-code"></i> HTML/CSS</li>
-              <li><i class="fas fa-code"></i> Git</li>
-              <li><i class="fas fa-code"></i> JavaScript</li>
-              <li><i class="fas fa-code"></i> React</li>
-              <li><i class="fas fa-code"></i> Node.js</li>
-              <li><i class="fas fa-code"></i> Express.js</li>
-              <li><i class="fas fa-code"></i> MongoDB</li>
-              <li><i class="fas fa-code"></i> MySQL</li>
-              <li><i class="fas fa-code"></i> APIs</li>
+    <section>
+      <Row>
+        <section className="section resume">
+          <div>
+            <h1 className="title resume  has-text-centered">Interested in Learning More?</h1>
+            <h2 className="subtitle"></h2>
 
-            </ul>
+            <div class="box">
+              <div className="mb-2">
+
+                <Button variant="light" size="lg">
+
+                  <a className="resume-anchor" style={{ color: "#000000" }} href={require('../../assets/janellmyrickResume.pdf').default} target="_blank" download >Download My Resume</a>
+                </Button>
+              </div>
+            </div>
+            <Col>
+              <h1 className="title resume  has-text-centered ">Skills</h1>
+              <div class="box skillBox columns">
+                <div class="columns ">
+                  <ul>
+                    <li><i class="fas fa-code"></i>Agile Methodologies</li>
+                    <li><i class="fas fa-code"></i>API/JSON</li>
+                    <li><i class="fas fa-code"></i>Bootstrap </li>
+                    <li><i class="fas fa-code"></i> Express.js</li>
+                    <li><i class="fas fa-code"></i> Git</li>
+                    <li><i class="fas fa-code"></i> HTML/CSS</li>
+                  </ul>
+                </div>
+                <div class="columns ">
+                  <ul>
+                    <li><i class="fas fa-code"></i> JavaScript</li>
+                    <li><i class="fas fa-code"></i>jQuery </li>
+                    <li><i class="fas fa-code"></i> MongoDB</li>
+                    <li><i class="fas fa-code"></i> Mongoose</li>
+                    <li><i class="fas fa-code"></i> MySQL</li>
+                    <li><i class="fas fa-code"></i> Node.js</li>
+                   
+                  </ul>
+                </div>
+                <div class="columns ">
+                  <ul>
+                  <li><i class="fas fa-code"></i> React</li>
+                    <li><i class="fas fa-code"></i> Responsive Design</li>
+                    <li><i class="fas fa-code"></i> Restful API </li>
+                    <li><i class="fas fa-code"></i> SEO </li>
+                    <li><i class="fas fa-code"></i>Social Coding Best Practices </li>
+                    <li><i class="fas fa-code"></i>State Management </li>
+                  </ul>
+                </div>
+              </div>
+
+
+            </Col>
           </div>
 
-        </h2>
-      </div>
+        </section>
+      </Row>
     </section>
   )
 }
